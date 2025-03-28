32a2cef79be556bdeef5a6d0f50ee03394af0a9c
shubham <shubham.raj2015@gmail.com>
2025-03-28 22:14:41 +0530

    initial commit

---
+++ blogPortal/src/appwrite/auth.js
@@ -0,0 +1,36 @@
+import { use } from "react";
+import config from "../config/config";
+import {Client, Account, ID} from "appwrite"
+
+
+export class AuthService {
+
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        
+        this.account = new Account(this.client)
+    }
+
+    async createAccount ({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(),
+            email, password, name);
+            if (userAccount) {
+                //call another method
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
